bb12e534192970ba32956eaf8e095bde25be9152
renho <rho_i_xx@163.com>
2015-09-20 16:16:03 +0800

    20150920
    add_time_ctr

--- sy/routes/utils.js
+++ sy/routes/utils.js
@@ -8,7 +8,6 @@ router.post('/', function(req, res, next) {
 });
 
 router.post('/getThisDate', function(req, res, next) {
-  //res.send(JSON.stringify({thisDate: (new Date()).format('yyyy-MM-dd hh:mm:ss.S')}));
-  res.send(JSON.stringify({thisDate: '2015-10-01 00:00:00:999'}));
+  res.send(JSON.stringify({thisDate: (new Date()).format('yyyy-MM-dd hh:mm:ss.S')}));
 });
 module.exports = router;
